f204e8e1a73e660a73e0f9e89afe394a9ab331ff
Stijn <stijn@code.nl>
2024-12-09 13:31:42 +0100

    Enhance Nuxt schema by adding a new appConfig group with fields for 'foo' and 'bar', including titles, descriptions, and icons for better organization and clarity.

--- nuxt.schema.ts
+++ nuxt.schema.ts
@@ -1,3 +1,20 @@
+import { field, group } from "@nuxthq/studio/theme";
+
 export default defineNuxtSchema({
-  appConfig: {},
+  appConfig: {
+    foo: group({
+      title: "Foo Group",
+      description: "Foo group description.",
+      icon: "i-mdi-palette-outline",
+      fields: {
+        bar: field({
+          type: "string",
+          title: "Bar title",
+          description: "Bar description.",
+          icon: "i-mdi-magnify",
+          default: "",
+        }),
+      },
+    }),
+  },
 });
